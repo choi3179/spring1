0bab08dd6ee8e2e069165c13a1ad511cdede636b
choi <jacobis@naver.com>
2023-04-13 05:22:36 +0900

    [ADD] 04.13 회원 도메인 설계

---
+++ src/main/java/com/spring1/core/MemberApp.java
@@ -0,0 +1,19 @@
+package com.spring1.core;
+
+import com.spring1.core.member.Grade;
+import com.spring1.core.member.Member;
+import com.spring1.core.member.MemberService;
+import com.spring1.core.member.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("find Member = " + findMember.getName());
+
+    }
+}
