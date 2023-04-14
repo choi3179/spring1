cc900b15f885f54169a6523f311a41c0f5b7d86b
choi <jacobis@naver.com>
2023-04-14 21:46:48 +0900

    [ADD] 04.14 AppConfig 스프링 기반 변경

--- src/main/java/com/spring1/core/OrderApp.java
+++ src/main/java/com/spring1/core/OrderApp.java
@@ -7,6 +7,8 @@ import com.spring1.core.member.MemberServiceImpl;
 import com.spring1.core.order.Order;
 import com.spring1.core.order.OrderService;
 import com.spring1.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
@@ -22,9 +24,13 @@ public class OrderApp {
 
         System.out.println("order = " + order);*/
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        //AppConfig appConfig = new AppConfig();
+        //MemberService memberService = appConfig.memberService();
+        //OrderService orderService = appConfig.orderService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
